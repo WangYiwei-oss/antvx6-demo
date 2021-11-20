bbe12bfd0364c454723e72a63138e7391c21ce50
WangYiwei-oss <wangyiwei@seu.edu.cn>
2021-11-20 19:50:43 +0800

    hello word

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+<!--    <router-view/>-->
+    <test></test>
+  </div>
+
+</template>
+
+
+
+<script>
+import test from "./components/test";
+export default {
+  name: 'App',
+  components:{
+    test
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
